e62bfe3cb3eede16faa617e36cda1f58d1271364
Miguel <miguelpellicergrau@gmail.com>
2021-05-22 19:04:51 +0200

    added image-labeling code

--- image-labeling.py
+++ image-labeling.py
@@ -4,7 +4,15 @@ import os
 import time
 import sys
 
-LABELING_PATH = os.path.join('resources', 'labelImg')
-if not os.path.exists(LABELING_PATH):
-    os.mkdir(LABELING_PATH)
-    os.system('git clone https://github.com/tzutalin/labelimg '+LABELING_PATH)
+LABELIMG_PATH = os.path.join('resources', 'labelImg')
+ABSOLUTE_LABELIMG_PATH = os.path.join(os.getcwd(), LABELIMG_PATH)
+if not os.path.exists(LABELIMG_PATH):
+    os.mkdir(LABELIMG_PATH)
+    os.system('git clone https://github.com/tzutalin/labelimg ' + LABELIMG_PATH)
+
+if os.name == 'posix':
+    os.system('cd' + ABSOLUTE_LABELIMG_PATH + '&& make qt5py3')
+if os.name == 'nt':
+    os.system('cd ' + ABSOLUTE_LABELIMG_PATH +' && pyrcc5 -o libs/resources.py resources.qrc')
+
+os.system('cd ' + ABSOLUTE_LABELIMG_PATH + ' && python labelImg.py')
